b16ff04e336121f78b9aaeca907b8cc331f4558a
Hanyu <hanyug@clemson.edu>
2017-03-25 20:54:45 -0400

    create folder and blob realated files

---
+++ include/blob.hpp
@@ -0,0 +1,14 @@
+#ifndef CREATINE_BLOB_HPP
+#define CREATINE_BLOB_HPP
+
+namespace creatine{
+
+class Blob {
+  public:
+    
+  protected:
+    
+};  // Blob class
+
+} // creatine name space
+#endif
